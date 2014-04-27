1e1fa0a53946f9ef1a5240de5f3615ee57f6d0da
treymarc <trey.marc@gmail.com>
2014-04-27 12:30:25 +0200

    correct printf in man

--- src/mavlink/man.c
+++ src/mavlink/man.c
@@ -2,15 +2,14 @@
 
 void rtfmVersion(const char * version)
 {
-    printf("\n\nVersion: ");
-    printf(version);
+    printf("\nVersion: %s", version);
     printf("\n\n");
 }
 
 void rtfmHelp(void)
 {
-    printf("\n\nmwgc - serial 2 udp");
-    printf("\n");
+    printf("\n\Multiwii Mavlink groundControl\n");
+
     printf("\nUsage:\n\n");
 
     printf("\t -ip <ip address of QGroundControl>\n");
@@ -21,14 +20,14 @@ void rtfmHelp(void)
     printf("\t  default value : 115200\n\n");
     printf("\t -id <id for mavlink>\n");
     printf("\t  default value : 1\n\n");
-    printf("\t  -telemetryauto <int>\n");
-    printf("\t   1 : assume the flight controler will send data\n");
-    printf("\t   0 : send request for each data (default)\n\n");
-    printf("\t  -hertz <int>\n");
-    printf("\t   Serial refresh for the imu. Default is 30, max is 60\n\n");
-    printf("\t--help");
+    printf("\t -telemetryauto <int>\n");
+    printf("\t  1 : assume the flight controler will send data\n");
+    printf("\t  0 : send request for each data (default)\n\n");
+    printf("\t -hertz <int>\n");
+    printf("\t  Serial refresh for the imu. Default is 30, max is 60\n\n");
+    printf("\t --help\n");
     printf("\t  display this message\n\n");
-    printf("\t --version");
+    printf("\t --version\n");
     printf("\t  show version number\n\n");
 }
 
